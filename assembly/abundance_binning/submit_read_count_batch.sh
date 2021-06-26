c765616c6cc934da887dfb4a4531d083edf14179
Li Zhang <61243260+lizhang12@users.noreply.github.com>
2021-06-26 18:24:36 +0800

    count mapped reads in each scaffolds and genome binning

--- assembly/abundance_binning/submit_read_count_batch.sh
+++ assembly/abundance_binning/submit_read_count_batch.sh
@@ -5,12 +5,10 @@ while read i
 do
 mkdir $i
 cp /work/TEDDY/assembly/metaspades/${i}/scaffolds.fasta  $i/${i}_scaffolds.fasta
-cp /work/TEDDY/script/summary/abundance_binning/read_count_new.batch $i/${i}_read_count.batch
+cp /work/TEDDY/script/summary/abundance_binning/read_count.batch $i/${i}_read_count.batch
 
 sed -i "s/SUBJECTID/$i/g"  $i/${i}_read_count.batch
-#sed -i "s/omicsbio/normal/g" $i/${i}_read_count.batch
-#sed  -i "s/time=48/time=72/g"   $i/${i}_read_count.batch
 sbatch  $i/${i}_read_count.batch
 
-done < /work/TEDDY/script/summary/abundance_binning/subjectID_list.txt
+done < subjectID_list.txt
 
