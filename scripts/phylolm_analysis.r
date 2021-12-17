331b3ae6e1a279e3672eba5ef650954c2946ff8b
Li Zhang <61243260+lizhang12@users.noreply.github.com>
2021-12-17 21:30:43 +0800

    create phylolm_analysis.r

--- scripts/phylolm_analysis.r
+++ scripts/phylolm_analysis.r
@@ -1,55 +1,54 @@
-# Rscript  FUS  g__Fusicatenibacter
-
-set.seed(123456)
 library(phylolm)
-
-args <- commandArgs(trailingOnly = TRUE)
-
-args[1] <- 'BAC'
-args[2] <- 'g__Bacteroides'
-dir <- paste0("/home/lizhang12/phylolm_test/",args[1])
-setwd(dir)
+set.seed(123456)
 
 # load tree
-tree_file <- paste0(args[1], "_faa.tree.nwk")
+tree_file <- "BAC_faa.tree.nwk" # tree file in newick
 tre <- read.tree(tree_file)
 
 tre$edge.length[which(tre$edge.length == 0)] <- 0.00000001
 # IA trait01
-ia_file <- paste0(args[2], "_genome_type.tsv")
+ia_file <- "g__Bacteroides_genome_type.tsv"
+############
+# two columns separted with Tab:
+# 1. MAGs and 2.their information of Seroconversion coded as "NOT" and "YES"
+# mags	groups
+# mag1	NOT
+############
 ia <- read.table(ia_file,head=T,sep="\t",stringsAsFactors = F)
-ia$module <- gsub("^","GMS_",ia$module)
 ia$trait01 <- ifelse(ia$groups == 'NOT',0,1)
-ia <- subset(ia,select = c(module,trait01))
+ia <- subset(ia,select = c(mags,trait01))
 
 # KEGG module
-modules <- paste0(args[2], "_genome_t.tsv")
+modules <- "g__Bacteroides_genome_t.tsv"
+###################################
+# the first column is mags, and followings are gene counts in each module
+# example:
+# mags M00064	M00652	M00797
+# mag_name 1	1	0
+###################################
 mkegg <- read.table(modules,head=T,sep="\t")
-mkegg$module <- gsub("^","GMS_",mkegg$module)
-row.names(mkegg) <- mkegg$module
+row.names(mkegg) <- mkegg$mags
 
-
-
-#### module M00570 ####
-kegg1 <-subset(mkegg,select = c(module,M00064))
+#### module M00064 ####
+kegg1 <-subset(mkegg,select = c(mags,M00064))
 # dat
-dat <- merge(ia,kegg1,by="module")
-row.names(dat) <- dat$module
+dat <- merge(ia,kegg1,by="mags")
+row.names(dat) <- dat$mags
 max_v <- max(dat$M00064)
 dat$M00064 <- dat$M00064/max_v
-dat <- subset(dat,select = c(trait01,M00064)) #M00024
+dat <- subset(dat,select = c(trait01,M00064)) #M00064
 
-fit <- phyloglm(trait01~M00064,phy=tre,data=dat,boot=100,btol = 20) #,
+fit <- phyloglm(trait01~M00064,phy=tre,data=dat,boot=100,btol = 20)
 
 result1 <- summary(fit)
 con1 <- as.data.frame(result1$coefficients)
 
 write.table(dat,"BAC_data.txt",sep="\t",quote = F)
-row.names(con1) <- c(paste0("M00570","_intercept"),"M00570")
+row.names(con1) <- c(paste0("M00064","_intercept"),"M00064")
 bac_result <- con1
 
 # KEGG module ### ALL ###
-#mkegg <- subset(mkegg,select = -c(module))
+#mkegg <- subset(mkegg,select = -c(mags))
 all_kegg <- colnames(mkegg)
 
 # max
@@ -58,15 +57,15 @@ max_value  <- apply(mkegg,2,max)
 #length(all_kegg)
 for (i in 2:length(all_kegg)){
   mod <- all_kegg[i]
-  kegg_tmp <-subset(mkegg,select = c("module",mod))
+  kegg_tmp <-subset(mkegg,select = c("mags",mod))
   if (max_value[i] == 0) {
     
     next;
   }else{
   kegg_tmp[,mod] <- kegg_tmp[,mod]/as.integer(max_value[i])
   # dat
-  dat_tmp <- merge(ia,kegg_tmp,by="module")
-  row.names(dat_tmp) <- dat_tmp$module
+  dat_tmp <- merge(ia,kegg_tmp,by="mags")
+  row.names(dat_tmp) <- dat_tmp$mags
   dat_tmp <- subset(dat_tmp,select = c("trait01",mod))
   names(dat_tmp) <- c("trait01","module_name")
   
@@ -81,5 +80,5 @@ for (i in 2:length(all_kegg)){
   bac_result <- rbind(bac_result,con_tmp)
 }}
 
-outfile <- paste0(args[1], "_phyloglm_all_max_btol20.txt")
+outfile <- "BAC_phyloglm_all_max_btol20.txt"
 write.table(bac_result,outfile,sep="\t",quote=F)
